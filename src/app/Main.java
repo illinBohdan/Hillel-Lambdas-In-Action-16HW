966726388cab954df0671a735779987a323a70d1
Bohdan <blazarchuk@gmail.com>
2025-11-14 10:42:16 +0100

    AddComplete tasks 1,2,3

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,5 +3,16 @@ package app;
 public class Main {
     public static void main(String[] args) {
 
+        MathOperation mathOperation = new MathOperation() {
+            @Override
+            public int operate(int a, int b) {
+                return a + b;
+            }
+        };
+        System.out.println(mathOperation.operate(5, 100));
+
+        StringManipulator sm = string -> string.toUpperCase();
+        System.out.println(sm.toUpperCase("rere"));
+
     }
 }
